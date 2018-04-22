615bffe5e070bbd7c7287090b40765b5465e0397
cnfive <108514901@qq.com>
2018-04-22 17:27:44 +0800

    add some files

---
+++ stock_history/stock_history/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace stock_history
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "获取股票交易历史数据";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(109, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(327, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(208, 190);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 47);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "获取股票代码和名称";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "股票代码：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(206, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 12);
+            this.label2.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 332);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 38);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "生成股票训练数据集";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 89);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(424, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(396, 190);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(168, 47);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "获取各大交易所历史指数";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(396, 267);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(168, 38);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "传媒影响力分析";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(396, 330);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(168, 40);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "预测股票价位";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 267);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(150, 35);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "获取公司股东组成";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(208, 266);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(136, 36);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "获取公司历史财务报表";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(12, 134);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(153, 31);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "获取股票单日交易明细";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(611, 190);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(133, 47);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "获取基金持股数据";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(611, 267);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(133, 39);
+            this.button11.TabIndex = 14;
+            this.button11.Text = "国家重大财经政策";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(208, 330);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(136, 40);
+            this.button12.TabIndex = 15;
+            this.button12.Text = "生成数据模型";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 382);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Alateng股票挖掘机";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+    }
+}
+
---
+++ stock_history/stock_history/Form1.cs
@@ -0,0 +1,504 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+
+using System.IO.Compression;
+using System.Web;
+
+using System.IO;
+
+using HtmlAgilityPack;
+
+using System.Text.RegularExpressions;
+
+using System.Threading; 
+
+namespace stock_history
+{
+    public partial class Form1 : Form
+    {
+        private int count=0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread oThread2 = new Thread(new ThreadStart(get_stock_history));
+                oThread2.Start(); // 程序运行的是Alpha.Beta()方法 
+            this.timer1.Interval = 1;//设置每隔千分之一秒钟执行一次
+            this.timer1.Enabled = true;
+            this.timer1.Start();
+
+        }
+    
+    private void get_stock_history()
+    {
+            string strCon = "Data Source=(local);database=lucene;uid=sa;pwd=qq901;";
+            DataClasses1DataContext db;
+            db = new DataClasses1DataContext(strCon);
+
+            string code = textBox1.Text.Trim();
+
+            for(int year=2006;year<2017;year++)
+            {
+                for (int jidu = 1; jidu < 5;jidu++ )
+                {
+                    string url = "http://money.finance.sina.com.cn/corp/go.php/vMS_MarketHistory/stockid/" + code + ".phtml?year=" + year.ToString() + "&jidu="+jidu.ToString();
+                    string htmlbody = GetPageData(url);
+                    //  Response.Write(htmlbody);
+                    //获取数据表格
+                    HtmlAgilityPack.HtmlDocument htmldoc = new HtmlAgilityPack.HtmlDocument();
+
+                    htmldoc.LoadHtml(htmlbody);
+
+                    HtmlNode rootNode = htmldoc.DocumentNode;
+
+                    string s_xpath = "//table[@id='FundHoldSharesTable']";
+
+                    string table = "";
+                    if (rootNode.SelectSingleNode(s_xpath) != null)
+                    {
+
+                        table = rootNode.SelectSingleNode(s_xpath).InnerHtml;
+
+                        //     Response.Write(table);
+
+                        //获取股票名称
+                        string xpath_name = "//thead[1]/tr[1]/th[1]";
+
+
+                        //获取行元素
+
+                        htmldoc.LoadHtml(table);
+
+                        rootNode = htmldoc.DocumentNode;
+
+                        string name = "";
+
+
+                        string stock_name = "";
+
+                        if (rootNode.SelectSingleNode(xpath_name) != null)
+                        {
+
+                            name = rootNode.SelectSingleNode(xpath_name).InnerText;
+                            name = name.Replace("年季度历史交易", "");
+                            //llll
+
+
+
+                            stock_name = name;
+                        }
+
+
+                        //获取商户节点
+                        string xpath_tr = "//tr";
+                        //   HtmlNode rootNode9 = htmldoc5.DocumentNode;
+                        HtmlNodeCollection trNodeList = rootNode.SelectNodes(xpath_tr);
+
+                        List<string> tr_l = new List<string>();
+                        foreach (HtmlNode d in trNodeList)
+                        {
+
+
+                            tr_l.Add(d.InnerHtml);
+
+                            //    Response.Write(d.InnerHtml);
+                        }
+                        foreach (string p in tr_l)
+                        {
+                            stock_trade_history stock = new stock_trade_history();
+                            stock.stock_name = stock_name;
+
+                            htmldoc.LoadHtml(p);
+                            rootNode = htmldoc.DocumentNode;
+
+                            s_xpath = "//td[1]/div[1]/a[1]";
+                            string riqi = "";
+                            if (rootNode.SelectSingleNode(s_xpath) != null)
+                            {
+
+                                riqi = rootNode.SelectSingleNode(s_xpath).InnerText;
+
+                                //llll
+
+                                //   Response.Write(riqi);
+                                stock.date = riqi;
+
+                            }
+                            s_xpath = "//td[2]/div[1]";
+                            string kaipanjia = "";
+
+                            if (rootNode.SelectSingleNode(s_xpath) != null)
+                            {
+
+                                kaipanjia = rootNode.SelectSingleNode(s_xpath).InnerText;
+
+                                //llll
+
+                                //  Response.Write(kaipanjia);
+
+                                stock.kaipanjia = kaipanjia;
+                            }
+                            s_xpath = "//td[3]/div[1]";
+                            string zuigaojia = "";
+
+                            if (rootNode.SelectSingleNode(s_xpath) != null)
+                            {
+
+                                zuigaojia = rootNode.SelectSingleNode(s_xpath).InnerText;
+
+                                //llll
+
+                                //  Response.Write(zuigaojia);
+
+                                stock.zuigaojia = zuigaojia;
+                            }
+
+                            s_xpath = "//td[4]/div[1]";
+                            string soupanjia = "";
+
+                            if (rootNode.SelectSingleNode(s_xpath) != null)
+                            {
+
+                                soupanjia = rootNode.SelectSingleNode(s_xpath).InnerText;
+
+                                //llll
+
+                                //    Response.Write(soupanjia);
+
+
+                                stock.soupanjia = soupanjia;
+                            }
+                            s_xpath = "//td[5]/div[1]";
+                            string zuidijia = "";
+
+                            if (rootNode.SelectSingleNode(s_xpath) != null)
+                            {
+
+                                zuidijia = rootNode.SelectSingleNode(s_xpath).InnerText;
+
+                                //llll
+
+                                //  Response.Write(zuidijia);
+
+                                stock.zuidijia = zuidijia;
+                            }
+
+                            s_xpath = "//td[6]/div[1]";
+                            string jiaoyiliang = "";
+
+                            if (rootNode.SelectSingleNode(s_xpath) != null)
+                            {
+
+                                jiaoyiliang = rootNode.SelectSingleNode(s_xpath).InnerText;
+
+                                //llll
+
+                                //  Response.Write(jiaoyiliang);
+                                stock.jiaoyiliang = jiaoyiliang;
+                            }
+
+                            s_xpath = "//td[7]/div[1]";
+                            string jiaoyijinge = "";
+
+                            if (rootNode.SelectSingleNode(s_xpath) != null)
+                            {
+
+                                jiaoyijinge = rootNode.SelectSingleNode(s_xpath).InnerText;
+
+                                //llll
+
+                                //    Response.Write(jiaoyijinge);
+                                stock.jiaoyijine = jiaoyijinge;
+                            }
+                            //提交数据
+                            if (riqi != "")
+                            {
+                                db.stock_trade_history.InsertOnSubmit(stock);
+
+                                ///提交插入操作
+                                db.SubmitChanges();
+                                count++;
+                            }
+                        }
+
+
+
+
+                    }
+
+                }
+
+            }
+            MessageBox.Show("获取结束！");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string strCon = "Data Source=(local);database=lucene;uid=sa;pwd=qq901;";
+            DataClasses1DataContext db;
+            db = new DataClasses1DataContext(strCon);
+            string url = "http://quote.eastmoney.com/stocklist.html";
+            string htmlbody = GetPageData(url);
+
+            //获取 链接地址和标题
+
+            string pattern5 = @"<a[\s\S]*?>[\s\S]*?</a>";
+            string pattern11 = @"(?<=href=[\u0022\u0027]?)(?!javascript)((?<Protocol>\w+):/)?[\/]?(?<Domain>[\w.]+\/?)((?![\u0022\u0027])\S)*";
+
+
+            ///定义获取标题的正则表达式
+            string pattern = @"(?<=<title\s*[^>]*>).*(?=<\/title>)";
+
+            string title = @"(?<=<a\s*[^>]*>)[\s\S]*?(?=<\/a>)";
+
+            string pattern_dd = @"<dd[\s\S]*?>[\s\S]*?</dd>";
+            htmlbody = htmlbody.Replace("<a name=\"sh\"/>","");
+             htmlbody = htmlbody.Replace("<a name=\"sz\"/>","");
+               MatchCollection matches_url_title = Regex.Matches(htmlbody, pattern5, RegexOptions.IgnoreCase);
+
+               foreach (Match m_u_t in matches_url_title)
+               {
+                   //寻找带下一页文字的链接全部
+                   if (m_u_t.Value.IndexOf("(") > -1)
+                   {
+
+                       MatchCollection matches_title = Regex.Matches(m_u_t.Value, title, RegexOptions.IgnoreCase);
+                       foreach(Match t in matches_title)
+                       {
+
+                           stock_id_name id_name = new stock_id_name();
+
+                      //     MessageBox.Show(t.Value);
+                           id_name.stock_name = t.Value;
+                           string strTest =t.Value;
+                           int startIndex = strTest.LastIndexOf("(") + 1;
+                         int length = strTest.Length - strTest.IndexOf(")");
+
+
+                           id_name.stock_id=strTest.Substring(startIndex, strTest.Length - length - startIndex);
+                           db.stock_id_name.InsertOnSubmit(id_name);
+
+                           ///提交插入操作
+                           db.SubmitChanges();
+                       }
+
+                   }
+
+               }
+               MessageBox.Show("ok!");
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private string GetPageData(string url)
+        {
+            StringBuilder s = new StringBuilder(102400);
+            WebClient wr = new WebClient();
+            wr.Headers[HttpRequestHeader.AcceptEncoding] = "gzip, deflate";
+            //   wr.Encoding = System.Text.Encoding.GetEncoding("utf-8");
+
+
+            //使用代理服务器
+
+            //   WebProxy proxyobject = new WebProxy("http://97.87.24.113:80", true);
+            //    wr.Proxy=proxyobject;
+
+            //模拟浏览器
+
+
+            wr.Headers.Add("Accept", "image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, */*");
+            wr.Headers.Add("Accept-Language", "zh-CN");
+            wr.Headers.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)");
+
+            //    wr.Headers.Add("User-Agent", "EtaoSpider");
+            //  byte[] buffer = wr.DownloadData(url);
+            byte[] buffer;
+        // 检查下载异常
+        t1: try
+            {
+
+                buffer = wr.DownloadData(url);
+
+            }
+
+            catch (Exception ex)
+            {
+
+
+                // MessageBox.Show(ex.Message);
+
+                //  Thread.Sleep(5000);
+
+                //     Delay_Second(1);
+
+                goto t1;
+
+
+
+            }
+
+
+
+            GZipStream g = new GZipStream((Stream)(new MemoryStream(buffer)), CompressionMode.Decompress);
+            byte[] d = new byte[20480];
+            int l = g.Read(d, 0, 20480);
+            while (l > 0)
+            {
+                s.Append(Encoding.Default.GetString(d, 0, l));
+                l = g.Read(d, 0, 20480);
+            }
+            //  Console.Write(s.ToString() + "\n\n\n" + s.Length);
+            Random d2 = new Random(DateTime.Now.Second);
+            int suiji;
+            suiji = d2.Next(5, 10);
+
+            //Thread.Sleep(suiji);
+            //    Delay_Second(suiji);
+
+            return (s.ToString());
+        }
+
+        private void Delay_Second(int second)
+        {
+            DateTime now = DateTime.Now;
+            while (now.AddSeconds(second) > DateTime.Now)
+            {
+                //程序等待中，等待指定的时间...
+                //    Application.DoEvents();     //释放CPU占用，以便CPU可以执行其它操作，不导致程序死掉-----根据实际使用。
+
+
+
+
+
+            }
+            //执行后面的程序
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label2.Text = "共获取" + count + "条股票交易信息！";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Thread oThread3 = new Thread(new ThreadStart(create_txt));
+            oThread3.Start(); // 程序运行的是Alpha.Beta()方法 
+            this.timer1.Interval = 1;//设置每隔千分之一秒钟执行一次
+            this.timer1.Enabled = true;
+            this.timer1.Start();
+
+        }
+
+        private void create_txt()
+
+    {
+            ///申明字符编码,命名空间System.Text
+            Encoding encoding = Encoding.GetEncoding("UTF-8");
+            string directory = "bbsmax";
+            string fileName = "stock.txt";
+            string templateUrl = "Templates/t.htm";
+            StreamReader sr = null;
+            ///StreamWriter实现一个 TextWriter，使其以一种特定的编码向流中写入字符，命名空间Syste.IO。
+            StreamWriter sw = null;
+            string strCon = "Data Source=(local);database=lucene;uid=sa;pwd=qq901;";
+            DataClasses1DataContext db;
+            db = new DataClasses1DataContext(strCon);
+
+            //清理未完成的站点链接，重新计算
+            IEnumerable<stock_trade_history> matches = from stock in db.stock_trade_history
+                                                           where stock.id > 0
+                                                           select stock;
+            if (matches.Count() > 0)
+            {
+                foreach(stock_trade_history stock in matches)
+                {
+
+                    // 创建文本文件
+
+                    try
+                    {
+                      //  string fileName2 = "fenlu" + j + ".aspx";
+
+                        ///保存新创建的HTML静态文件
+                   //     sw = new StreamWriter( fileName, false, encoding);
+                       // if (sw == null) return false;
+                //        sw.Write(stock.kaipanjia);
+                        ///清理缓存区
+                   //     sw.Flush();
+
+                        string txt = stock.stock_name.Trim()+"  "+stock.kaipanjia+"  "+stock.zuigaojia+"  "+stock.soupanjia+"  "+stock.zuidijia+"  "+stock.jiaoyiliang+"  "+stock.jiaoyijine+"  "+stock.date.Trim();
+
+                        if(!File.Exists("stock.txt"))
+                        {
+                            sw = File.CreateText("stock.txt");
+                       //不换行
+                            //   sw.Write("");
+                            sw.WriteLine(txt);
+                            count++;
+
+
+                        }
+                        else
+                        {
+                            //如果文件存在就 添加一些内容
+
+                            sw = File.AppendText("stock.txt");
+                            sw.WriteLine(txt);
+                            count++;
+
+
+
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                       // HttpContext.Current.Response.Write(ex.Message);
+                       // HttpContext.Current.Response.End();
+                    }
+                    finally
+                    {
+                        sw.Close();
+                        sw.Dispose();
+                    }
+
+                }
+
+            }
+            MessageBox.Show("数据集生成完成！");
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
